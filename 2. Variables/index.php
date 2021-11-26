b10ad8d51645b1426b320155699b6f8788cb0f64
RysteQ <34135554+RysteQ@users.noreply.github.com>
2021-11-26 02:11:09 +0200

    Initial commit
    Some basic concepts I have learned so far, PHP doesn't seem hard or bad after all (I said bad because it has a reputation in my group of people)

---
+++ 2. Variables/index.php
@@ -0,0 +1,10 @@
+<?php
+    // variables
+    $numOne = 9;
+    $numTwo = 7;
+    $goodbyeMessage = "Goodbye !";
+
+    // echo statements
+    echo "$numOne + $numTwo = " . ($numOne + $numTwo) . "<br><br><br>";
+    echo $goodbyeMessage;
+?>
---
+++ 3. Constants/index.php
@@ -0,0 +1,5 @@
+<?php
+    define("TRUTH", "You shall not pass !", true);
+
+    echo TRUTH;
+?>
